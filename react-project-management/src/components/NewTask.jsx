bc3be6990dfe6a13f178974cea76c995c76a4f3a
pandalow <zxj000hugh@gmail.com>
2024-07-31 12:45:01 +0100

    feat: adding docker nginx file

--- react-project-management/src/components/NewTask.jsx
+++ react-project-management/src/components/NewTask.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 export default function NewTask({ onAdd }) {
-    const [enteredTask, setEnteredTask] = useState()
+    const [enteredTask, setEnteredTask] = useState('')
 
     function handleChange(event) {
         setEnteredTask(event.target.value)
@@ -17,7 +17,8 @@ return <div className="flex items-center gap-4">
         type='text'
         className="w-64 px-2 py-1 rounded-sm bg-stone-200"
         onChange={handleChange}
-        value={enteredTask} />
-    <button onClick={handleClick} className=" text-stone-700 hover:text-stone-900" />
+        value={enteredTask} 
+        />
+    <button onClick={handleClick} className="text-stone-700 hover:text-stone-900"> Add Task</button> 
 </div>
 }
--- react-project-management/src/components/ProjectDetails.jsx
+++ react-project-management/src/components/ProjectDetails.jsx
@@ -17,8 +17,10 @@ const ProjectDetails = forwardRef(function ProjectDetails({ project, onDelete, o
                     <h1 className='text-3xl font-bold text-stone-600 mb-2'>{project.title}</h1>
                     <button className='text-600 hover:text-stone-950' onClick={onDelete}>Delete</button>
                 </div>
-                <p className='text-stone-600 whitespace-pre-wrap'>{project.description}</p>
-                <p className='text-stone-600'  >{formattedDate}</p>
+                <div className='flex flex-col'>
+                <p className='text-stone-600 whitespace-pre-wrap mb-2'>{project.description}</p>
+                <p className='text-stone-600 mb-2'  >{formattedDate}</p>
+                </div>
             </header>
             <Tasks onAdd={onAddTask} onDelete={onDeleteTask} tasks={tasks} />
 
